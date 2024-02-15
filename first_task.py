c2a9f28d4bd893c9b32b40d2e490547895dbce71
amirgar <garam08@bk.ru>
2024-02-15 16:58:08 +0300

    Добавление решения первой задачи

---
+++ first_task.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    sample_text = ""
+    return render_template("base.html", title=title, sample_text=sample_text)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1', debug=True)
